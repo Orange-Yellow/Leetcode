187618c750e91b5a3cd7ee5a241be069d83619ea
Orange-Yellow <625528743@qq.com>
2022-05-17 10:39:33 +0800

    2022-05-16 每日一题（递归）

--- Question of the day/2022/05/2022-05-16（面试题 04.06. 后继者）/main.go
+++ Question of the day/2022/05/2022-05-16（面试题 04.06. 后继者）/main.go
@@ -19,7 +19,8 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func inorderSuccessor(root *TreeNode, p *TreeNode) *TreeNode {
+// 官方题解
+func InorderSuccessor(root *TreeNode, p *TreeNode) *TreeNode {
 	var ans *TreeNode
 	//《左->根->右》
 	// 如果p的右节点不等于空则p的【下一个节点在p的右节点的最左节点】
@@ -44,4 +45,22 @@ func inorderSuccessor(root *TreeNode, p *TreeNode) *TreeNode {
 		}
 	}
 	return ans
+}
+
+// 根据BFS特性 递归题解
+func inorderSuccessor(root *TreeNode, p *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	// 如果root小于p 说明，下一节点在root的右节点
+	if root.Val <= p.Val {
+		return inorderSuccessor(root.Right, p)
+	}
+	// 如果左节点为空，说明是root本身
+	ans := inorderSuccessor(root.Left, p)
+	if ans == nil {
+		return root
+	} else {
+		return ans
+	}
 }
