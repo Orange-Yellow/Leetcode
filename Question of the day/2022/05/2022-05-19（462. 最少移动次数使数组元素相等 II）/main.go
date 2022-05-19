415a9207112944d51b996718a76a343b73ed38d0
Orange-Yellow <625528743@qq.com>
2022-05-19 13:27:09 +0800

    2022-05-19 每日一题

---
+++ Question of the day/2022/05/2022-05-19（462. 最少移动次数使数组元素相等 II）/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+/*
+	name: 462. 最少移动次数使数组元素相等 II
+	link: https://leetcode.cn/problems/minimum-moves-to-equal-array-elements-ii/
+*/
+
+func main() {
+	x := []int{1, 10, 2, 9}
+	fmt.Println(minMoves2(x))
+}
+
+func minMoves2(nums []int) int {
+	sort.Ints(nums)
+	mid := nums[len(nums)/2]
+	ans := 0
+	for i := range nums {
+		if nums[i]-mid < 0 {
+			ans += -(nums[i] - mid)
+		}
+		ans += nums[i] - mid
+	}
+	return ans
+}
