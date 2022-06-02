c7605974b5314ec57528bec6b4433ba71553bc00
Orange-Yellow <625528743@qq.com>
2022-06-02 20:35:40 +0800

    2022-06-02 每日一题

---
+++ Question of the day/2022/06/2022-06-02（450. 删除二叉搜索树中的节点)/main.go
@@ -0,0 +1,41 @@
+package main
+
+/*
+	name: 450. 删除二叉搜索树中的节点
+	linke: https://leetcode.cn/problems/delete-node-in-a-bst/
+*/
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func deleteNode(root *TreeNode, key int) *TreeNode {
+	switch {
+	case root == nil:
+		return nil
+	case root.Val > key:
+		root.Left = deleteNode(root.Left, key)
+	case root.Val < key:
+		root.Right = deleteNode(root.Right, key)
+	case root.Left == nil || root.Right == nil:
+		if root.Left != nil {
+			return root.Left
+		}
+		return root.Right
+	default:
+		successor := root.Right
+		for successor.Left != nil {
+			successor = successor.Left
+		}
+		successor.Right = deleteNode(root.Right, successor.Val)
+		successor.Left = root.Left
+		return successor
+	}
+	return root
+}
