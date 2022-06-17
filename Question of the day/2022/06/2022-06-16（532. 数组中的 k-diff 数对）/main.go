f7bb7f2e7bd20d4d4a5d384f391a7665342005ee
Orange-Yellow <625528743@qq.com>
2022-06-17 21:34:48 +0800

    2022-06-14 每日一题

---
+++ Question of the day/2022/06/2022-06-16（532. 数组中的 k-diff 数对）/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "sort"
+
+/*
+	name: 532. 数组中的 k-diff 数对
+	link:
+*/
+func main() {
+
+}
+
+func findPairs(nums []int, k int) (ans int) {
+	sort.Ints(nums)
+	y, n := 0, len(nums)
+	for x, num := range nums {
+		if x == 0 || num != nums[x-1] {
+			for y < n && (nums[y] < num+k || y <= x) {
+				y++
+			}
+			if y < n && nums[y] == num+k {
+				ans++
+			}
+		}
+	}
+	return
+}
