8e8971714ec68a24bb9801906c10fbb402cdafc7
Orange-Yellow <625528743@qq.com>
2022-07-21 21:18:13 +0800

    2022-07-21 每日一题

---
+++ Question of the day/2022/07/2022-07-21（814. 二叉树剪枝）/main.go
@@ -0,0 +1,28 @@
+package main
+
+/*
+	name: 814. 二叉树剪枝
+	link: https://leetcode.cn/problems/binary-tree-pruning/
+*/
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func pruneTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left = pruneTree(root.Left)
+	root.Right = pruneTree(root.Right)
+	if root.Left == nil && root.Right == nil && root.Val == 0 {
+		return nil
+	}
+	return root
+}
