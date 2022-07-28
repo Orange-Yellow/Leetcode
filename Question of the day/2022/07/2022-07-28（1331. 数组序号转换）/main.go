47594aa3d058da78826249835a7226bf2698091b
Orange-Yellow <625528743@qq.com>
2022-07-28 21:37:12 +0800

    2022-07-28 每日一题

---
+++ Question of the day/2022/07/2022-07-28（1331. 数组序号转换）/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "sort"
+
+/*
+	name: 1331. 数组序号转换
+	link: https://leetcode.cn/problems/rank-transform-of-an-array/
+*/
+
+func main() {
+
+}
+
+func arrayRankTransform(arr []int) []int {
+	a := append([]int{}, arr...)
+	sort.Ints(a)
+	ranks := map[int]int{}
+	for _, v := range a {
+		if _, ok := ranks[v]; !ok {
+			ranks[v] = len(ranks) + 1
+		}
+	}
+	for i, v := range arr {
+		arr[i] = ranks[v]
+	}
+	return arr
+}
