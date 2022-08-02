274724ec4a3872b9422e5da83a86d55297d4f284
Orange-Yellow <625528743@qq.com>
2022-08-02 21:30:27 +0800

    2022-08-01 每日一题

---
+++ Question of the day/2022/08/2022-08-02（622. 设计循环队列）/main.go
@@ -0,0 +1,58 @@
+package main
+
+/*
+	name: 622. 设计循环队列
+	link: https://leetcode.cn/problems/design-circular-queue/
+*/
+
+func main() {
+
+}
+
+type MyCircularQueue struct {
+	front, rear int
+	elements    []int
+}
+
+func Constructor(k int) MyCircularQueue {
+	return MyCircularQueue{elements: make([]int, k+1)}
+}
+
+func (q *MyCircularQueue) EnQueue(value int) bool {
+	if q.IsFull() {
+		return false
+	}
+	q.elements[q.rear] = value
+	q.rear = (q.rear + 1) % len(q.elements)
+	return true
+}
+
+func (q *MyCircularQueue) DeQueue() bool {
+	if q.IsEmpty() {
+		return false
+	}
+	q.front = (q.front + 1) % len(q.elements)
+	return true
+}
+
+func (q MyCircularQueue) Front() int {
+	if q.IsEmpty() {
+		return -1
+	}
+	return q.elements[q.front]
+}
+
+func (q MyCircularQueue) Rear() int {
+	if q.IsEmpty() {
+		return -1
+	}
+	return q.elements[(q.rear-1+len(q.elements))%len(q.elements)]
+}
+
+func (q MyCircularQueue) IsEmpty() bool {
+	return q.rear == q.front
+}
+
+func (q MyCircularQueue) IsFull() bool {
+	return (q.rear+1)%len(q.elements) == q.front
+}
